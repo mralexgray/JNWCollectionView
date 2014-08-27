33cf183966785bfa5fac5f1120a3866c777e1efe
Alex Gray <alex@mrgray.com>
2014-08-27 09:46:51 -0400

    List Demo colored gradient cell support.
    Also, bound  headerLabel.stringValue to headerLabelText vs separate method call.  autoresizing of header text field vs. layout method.. layout method with call to super left intact for reference / demo.

--- demo/JNWCollectionViewDemo/ListDemoViewController.m
+++ demo/JNWCollectionViewDemo/ListDemoViewController.m
@@ -4,68 +4,75 @@
 #import "ListHeader.h"
 #import "ListCell.h"
 
-@interface ListDemoViewController ()
-@property (nonatomic) JNWCollectionView *collectionView;
-@end
-
 static NSString * const cellIdentifier    = @"CELL",
                 * const headerIdentifier  = @"HEADER";
 
-@implementation ListDemoViewController
+@implementation ListDemoViewController { NSMutableArray *colors; JNWCollectionView *collectionView; }
 
-- (id)init { return [super initWithNibName:nil bundle:nil]; }
+- init { self = [super initWithNibName:nil bundle:nil];
 
-- (void)loadView {
+    colors = @[].mutableCopy;
+    for (float i = 0; i < 360; i++) {
+      [colors addObject:[NSColor colorWithDeviceHue:i/360. saturation:1 brightness:1 alpha:1]];
+    }
+    return self;
+}
+
+- (void) loadView {
 
 	self.view = [NSView.alloc initWithFrame:CGRectZero];
 	self.view.wantsLayer = YES;
 	
-	[self.view addSubview:self.collectionView = [JNWCollectionView.alloc initWithFrame:self.view.bounds]];
-	self.collectionView.autoresizingMask      = NSViewWidthSizable | NSViewHeightSizable;
-	self.collectionView.dataSource            = self;
-	JNWCollectionViewListLayout *layout       = JNWCollectionViewListLayout.new;
-//	layout.rowHeight                          = 44.f;
-	layout.delegate                           = self;
-  layout.stickyHeaders                      = YES;
-	self.collectionView.collectionViewLayout  = layout;
+	[self.view addSubview:collectionView = [JNWCollectionView.alloc initWithFrame:self.view.bounds]];
+	collectionView.autoresizingMask      = NSViewWidthSizable | NSViewHeightSizable;
+	collectionView.dataSource            = self;
+	JNWCollectionViewListLayout *layout  = JNWCollectionViewListLayout.new;
+//	layout.rowHeight                   = 44.f;
+	layout.delegate                      = self;
+  layout.stickyHeaders                 = YES;
+	collectionView.collectionViewLayout  = layout;
 	
-	[self.collectionView registerClass:ListCell.class   forCellWithReuseIdentifier:cellIdentifier];
-	[self.collectionView registerClass:ListHeader.class forSupplementaryViewOfKind:JNWCollectionViewListLayoutHeaderKind withReuseIdentifier:headerIdentifier];
+	[collectionView registerClass:ListCell.class   forCellWithReuseIdentifier:cellIdentifier];
+	[collectionView registerClass:ListHeader.class forSupplementaryViewOfKind:JNWCollectionViewListLayoutHeaderKind withReuseIdentifier:headerIdentifier];
 	
-	self.collectionView.animatesSelection     = YES;
-	[self.collectionView reloadData];
+	collectionView.animatesSelection     = YES;
+	[collectionView reloadData];
 }
 
-- (JNWCollectionViewCell*) collectionView:(JNWCollectionView*)cV cellForItemAtIndexPath:(NSIndexPath*)iP {
-	ListCell *cell = (ListCell *)[cV dequeueReusableCellWithIdentifier:cellIdentifier];
-	cell.cellLabelText = [NSString stringWithFormat:@"%ld - sect:%ld row:%lu", iP.jnw_item, iP.jnw_section,iP.length];
+- (JNWCollectionViewCell*) collectionView:(JNWCollectionView*)v cellForItemAtIndexPath:(NSIndexPath*)iP {
+
+	ListCell *cell       = (id)[v dequeueReusableCellWithIdentifier:cellIdentifier];
+	cell.cellLabelText   = [NSString stringWithFormat:@"%ld - sect:%ld row:%lu (%@)", iP.jnw_item, iP.jnw_section,iP.length, iP];
+  cell.backgroundColor = colors[iP.jnw_item];
 	return cell;
 }
 
-- (JNWCollectionViewReusableView*) collectionView:(JNWCollectionView*)cV viewForSupplementaryViewOfKind:(NSString*)k inSection:(NSInteger)sct {
-	ListHeader *header     = (ListHeader*)[cV dequeueReusableSupplementaryViewOfKind:k withReuseIdentifer:headerIdentifier];
-	header.headerLabelText = [NSString stringWithFormat:@"Header %ld", sct];
+- (JNWCollectionViewReusableView*) collectionView:(JNWCollectionView*)v viewForSupplementaryViewOfKind:(NSString*)k inSection:(NSInteger)s {
+
+	ListHeader *header     = (id)[v dequeueReusableSupplementaryViewOfKind:k withReuseIdentifer:headerIdentifier];
+	header.headerLabelText = [NSString stringWithFormat:@"Header %ld - %@ Colors", s, s ? @"Cooler" : @"Warmer"];
 	return header;
 }
 
-// Can be used in place of setting the collective height of the layout, as seen above in `-loadView`, if
-// a variable-row height list view is wanted.
+// Can be used in place of setting the collective height of the layout, as seen above in `-loadView`, if a variable-row height list view is wanted.
 
-- (CGFloat)collectionView:(JNWCollectionView *)collectionView heightForRowAtIndexPath:(NSIndexPath*)indexPath {
+- (CGFloat)collectionView:(JNWCollectionView*)v heightForRowAtIndexPath:(NSIndexPath*)iP {
 
     return (arc4random() % 109) + 44.f;
 }
 
-- (NSUInteger)collectionView:(JNWCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-	return 300;
+- (NSUInteger)collectionView:(JNWCollectionView*)v numberOfItemsInSection:(NSInteger)s {
+
+  return colors.count/2;
 }
 
-- (NSInteger)numberOfSectionsInCollectionView:(JNWCollectionView *)collectionView {
-	return 5;
+- (NSInteger)numberOfSectionsInCollectionView:(JNWCollectionView*)v {
+
+  return 2;
 }
 
-- (CGFloat)collectionView:(JNWCollectionView *)collectionView heightForHeaderInSection:(NSInteger)index {
-	return 24.f;
+- (CGFloat)collectionView:(JNWCollectionView*)v heightForHeaderInSection:(NSInteger)index {
+	return 24;
 }
 
 @end
